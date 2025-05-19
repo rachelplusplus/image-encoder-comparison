2d863bd6cd960e6f5d2071adf60b2fecec7c4d0d
Rachel Barker <rachel@rachelplusplus.me.uk>
2025-05-19 08:00:51 +0000

    Add script to generate graphs for the upcoming blog post
    This absorbs and replaces the previous setup_input.py script

---
+++ generate_plots.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eux
+
+SOURCE_DIR=../test-videos
+
+if ! [ -e big_buck_bunny_f231_1080.y4m ]; then
+  # TODO: Check SHA256 checksum of the resulting file?
+  ffmpeg -i $SOURCE_DIR/big_buck_bunny_1080p24.y4m \
+         -vf 'select=eq(n\,231)' -fps_mode passthrough -frames:v 1 \
+         -loglevel warning \
+         big_buck_bunny_f231_1080.y4m
+fi
+
+./encode.py tinyavif-1.1 tinyavif big_buck_bunny_f231_1080.y4m
+./encode.py jpegli-0.11.1 jpegli big_buck_bunny_f231_1080.y4m
+./encode.py aom-3.12.1 libaom big_buck_bunny_f231_1080.y4m
+
+./plot.py -s big_buck_bunny_f231_1080.y4m tinyavif-1.1 jpegli-0.11.1 aom-3.12.1
